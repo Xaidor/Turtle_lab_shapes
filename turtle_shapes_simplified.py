b5ef0dca9bf16e4a36be7df440ae0b540887a7a4
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2026-02-11 16:47:46 -0500

    If statment square

--- turtle_shapes_simplified.py
+++ turtle_shapes_simplified.py
@@ -1,46 +1,35 @@
 # Import Libraries 
 import turtle as t
-import numpy as np
+import random as r
 
-# Declare Variables 
+# Declare Variables Not needed, just for practice purposes
 select_shape = str()
 square_shape = str()
 star_shape = str()
 sun_shape = str()
 select_color = str()
-num_rays = int()
 
 # Input statement 
 select_shape = input("Select a shape (Star, Square, or Sun ")
 select_color = input("What color would you like your shape? ")
 
-# Proccess
-num_rays = 7
+# Set color
+t.color(select_color)
+
+# square shape 
+if select_shape == "square":
+    t.penup()
+    t.goto(-50,-50)
+    t.pendown()
+    
+    for i in range(4): 
+        t.forward(100)
+        t.right(90)
+
+
 
 # Draw circle for sun
-if select_shape not 'star' | "square":
-    t.penup()
-    t.goto(0, -50)
-    t.pendown()
-    t.color(select_color)
-    t.begin_fill()
-    t.circle(70)
-    t.end_fill()
-# Draw sun rays 
-t.penup()
-t.goto(0,0)
-t.pendown()
 
-for ray in range(num_rays):
-    t.penup()
-    t.forward(70)
-    t.pendown()
-    t.forward(30)
-    t.penup()
-    
-    for i in range(num_rays):
-        t.left(60)
-        t.forward(70)   
 
 
 # Add sun rays to circle
